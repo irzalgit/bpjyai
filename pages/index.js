21751c1467704f543b02aa285afcbcb65c2b6027
mathjsx <irzalpa23@outlook.com>
2024-12-11 22:35:42 +0700

    next

---
+++ pages/index.js
@@ -0,0 +1,19 @@
+
+
+
+// pages/index.js
+
+import Head from 'next/head';
+
+export default function Home() {
+  return (
+    <div>
+      <Head>
+        <title>Welcome</title>
+      </Head>
+      <main className="container mx-auto mt-10">
+        <h1 className="text-4xl font-bold text-center">Welcome</h1>
+      </main>
+    </div>
+  );
+}
